5f4f69acb01410c6dc889eb73f4bd2a7ed3e573b
anjaneyulu-01 <anjaneyulu.dev01@gamil.com>
2026-09-10 13:04:49 +0530

    Practical-6 completed

--- Practical-6/matrixChainMultiplication.cpp
+++ Practical-6/matrixChainMultiplication.cpp
@@ -1,20 +1,24 @@
 #include <iostream>
 #include <vector>
 #include <climits>
+
 using namespace std;
 
-int matrixChainMultiplication(vector<int>& p) {
-    int n = p.size() - 1;  
+int matrixChainMultiplication(vector<int>& p)
+{
+    int n = p.size() - 1;
+
     vector<vector<int>> dp(n + 1, vector<int>(n + 1, 0));
 
-  
-    for (int len = 2; len <= n; len++) {
-        for (int i = 1; i <= n - len + 1; i++) {
-
+    for (int len = 2; len <= n; len++)
+    {
+        for (int i = 1; i <= n - len + 1; i++)
+        {
             int j = i + len - 1;
             dp[i][j] = INT_MAX;
-            for (int k = i; k < j; k++) {
 
+            for (int k = i; k < j; k++)
+            {
                 int cost = dp[i][k]
                          + dp[k + 1][j]
                          + p[i - 1] * p[k] * p[j];
@@ -27,14 +31,21 @@ int matrixChainMultiplication(vector<int>& p) {
     return dp[1][n];
 }
 
-int main() {
-    // Matrices:
-    // A1 = 10 x 20
-    // A2 = 20 x 30
-    // A3 = 30 x 40
-    // A4 = 40 x 30
+int main()
+{
+    int n;
 
-    vector<int> p = {10, 20, 30, 40, 30};
+    cout << "Enter number of matrices: ";
+    cin >> n;
+
+    vector<int> p(n + 1);
+
+    cout << "Enter " << n + 1 << " dimensions: ";
+
+    for (int i = 0; i <= n; i++)
+    {
+        cin >> p[i];
+    }
 
     cout << "Minimum number of multiplications = "
          << matrixChainMultiplication(p) << endl;
